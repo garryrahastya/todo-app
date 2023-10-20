1851565d3f2059231377fee016bbe8fd5a22332e
Garry Rahastya <garrybrahastya@gmail.com>
2023-10-20 11:44:33 +0700

    Ganti Judul

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { v4 as uuidv4 } from "uuid"; // Import library uuid
 
-import style from "./TodoList.module.css"
+import style from "./TodoList.module.css";
 
 function TodoList() {
   const [todos, setTodos] = useState([]);
@@ -18,12 +18,11 @@ function TodoList() {
       setTodos([...todos, newTodo]);
       setInputValue("");
       setError(null);
-    }
-    else {
-        setError("Input harus diisi!")
-        setTimeout(() => {
-            setError(null);
-          }, 2000);
+    } else {
+      setError("Input harus diisi!");
+      setTimeout(() => {
+        setError(null);
+      }, 2000);
     }
   };
 
@@ -45,37 +44,44 @@ function TodoList() {
   return (
     <div className={style.container}>
       <h1>To Do App</h1>
-      <br/>
-      {/* {error && <div className={style.error}>{error}</div>} */}
+      <br />
       <div className={style["input-container"]}>
-      <input
-        type="text"
-        value={inputValue}
-        onChange={handleInputChange}
-        placeholder={error ? error : 'Add to do...'}
-        className={error ? `${style.input} ${style.error}` : style.input}
-      />
-      <button className={style["add-button"]}onClick={handleAddTodo}>Add Todo</button>
-      
+        <input
+          type="text"
+          value={inputValue}
+          onChange={handleInputChange}
+          placeholder={error ? error : "Add to do..."}
+          className={error ? `${style.input} ${style.error}` : style.input}
+        />
+        <button className={style["add-button"]} onClick={handleAddTodo}>
+          Add Todo
+        </button>
       </div>
+      <h1>Tasks</h1>
       <ul className={style["todo-list"]}>
         {todos.map((todo) => (
           <li key={todo.id} className={style["todo-item"]}>
-            <button className={style["delete-button"]} onClick={() => handleDeleteTodo(todo.id)}>Delete</button>
+            <button
+              className={style["delete-button"]}
+              onClick={() => handleDeleteTodo(todo.id)}
+            >
+              Delete
+            </button>
             <span
               style={{
                 textDecoration: todo.completed ? "line-through" : "none",
-                opacity: todo.completed ? "0.3" : "1"
+                opacity: todo.completed ? "0.3" : "1",
               }}
             >
-                
               {todo.value}
             </span>
-            
-            <button className={style["toggle-button"]} onClick={() => handleToggleTodo(todo.id)}>
+
+            <button
+              className={style["toggle-button"]}
+              onClick={() => handleToggleTodo(todo.id)}
+            >
               {todo.completed ? "Undone?" : "Done?"}
             </button>
-            
           </li>
         ))}
       </ul>
